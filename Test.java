67f7392158d01f15b2ce5d47483c2eec94f5e070
AsiriZenith <asirisenith@gmail.com>
2021-06-03 14:47:41 +0530

    After modify Test.java

--- Test.java
+++ Test.java
@@ -2,4 +2,8 @@ class Test{
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    public void function1() {
+        System.out.println("Inside the function 1");
+    }
 }
